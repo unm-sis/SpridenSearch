513f05965165c02ce7b4fb2c257ba97f0e3ea2e8
Jason Foutz <jwfoutz@unm.edu>
2013-11-08 16:58:24 -0700

    fix autowired beans

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,18 @@
-// Place your Spring DSL code here
+import org.springframework.jdbc.datasource.UserCredentialsDataSourceAdapter
+import org.springframework.jdbc.support.lob.OracleLobHandler
+import org.springframework.jdbc.support.nativejdbc.CommonsDbcpNativeJdbcExtractor
+
+
+//SCC 2013-11-01: Added the following from Jason's code
+//This allowed for the application to start but I still got an error:
+//| Error 2013-11-01 15:06:38,845 [localhost-startStop-1] ERROR spring.GrailsRuntimeConfigurator  - [RuntimeConfiguration] Unable to load beans from resources.groovy
+//Message: No such property: UserDatabaseAuthenticationProvider for class: resources
 beans = {
+	lobHandlerDetector(OracleLobHandler) { nativeJdbcExtractor = new CommonsDbcpNativeJdbcExtractor() }
+
+	dataSource(UserCredentialsDataSourceAdapter){ targetDataSource= ref("dataSourceUnproxied") }
+
+	dbAuthenticationProvider(UserDatabaseAuthenticationProvider){ dataSource = ref("dataSource") }
+
 }
+
